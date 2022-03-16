63995d1505b0e6cf70d390f487c9ec8b7c723178
ParimalSAgarkhed <44961436+ParimalSAgarkhed@users.noreply.github.com>
2022-03-16 13:15:06 +0530

    Add files via upload

---
+++ BethanysPieShop-master/BethanysPieShop/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BethanysPieShop
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. 
+        // Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IPieRepository, MockPieRepository>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // The order in which you add middleware components matters.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+
+            // Enable "Yellow Screen of Death" for more feedback.
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            //}
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+        }
+    }
+}
